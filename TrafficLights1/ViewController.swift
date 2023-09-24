771e0e9895fbb6edaf6bdd8848b50a9138580e8b
Elizaveta <elizavetash0210@mail.ru>
2023-09-24 22:07:25 +0300

    new homework

--- TrafficLights1/ViewController.swift
+++ TrafficLights1/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstLight: UIView!
+    @IBOutlet weak var secondLight: UIView!
+    @IBOutlet weak var thirdLight: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        firstLight.alpha = 0.5
+        secondLight.alpha = 0.5
+        thirdLight.alpha = 0.5
+        
+        firstLight.layer.cornerRadius = 75
+        secondLight.layer.cornerRadius = 75
+        thirdLight.layer.cornerRadius = 75
+        
+        startButton.layer.cornerRadius = 10
+        
+        startButton.setTitle("START", for: .normal)
+        
     }
 
-
+    @IBAction func startButtonPressed() {
+        
+        if firstLight.alpha == 0.5 {
+            startButton.setTitle("NEXT", for: .normal)
+            firstLight.alpha = 1
+        }
+    }
+    
+    @IBAction func nextTwoButtonPressed() {
+        if firstLight.alpha == 1 {
+            startButton.setTitle("NEXT", for: .normal)
+            secondLight.alpha = 1
+            firstLight.alpha = 0.5
+            thirdLight.alpha = 0.5
+        }
+    }
+    
+    @IBAction func nextThreeButtonPressed() {
+        if secondLight.alpha == 1 {
+            startButton.setTitle("NEXT", for: .normal)
+            thirdLight.alpha = 1
+            firstLight.alpha = 0.5
+            secondLight.alpha = 0.5
+        }
+    }
+    
 }
 
