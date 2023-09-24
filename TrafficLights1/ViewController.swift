699cc63254816677567356a3f36789dd9b57eb02
Elizaveta <elizavetash0210@mail.ru>
2023-09-24 19:43:55 +0300

    Initial Commit

---
+++ TrafficLights1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLights1
+//
+//  Created by Елизавета Шалдыбина on 24.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
